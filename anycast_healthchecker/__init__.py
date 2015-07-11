a2b91435d65faa85aa37970268bc14dc719db8dd
Pavlos Parissis <pavlos.parissis@booking.com>
2015-07-12 00:08:31 +0200

    add some useful info in __init__

---
+++ anycast_healthchecker/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+
+__title__ = 'anycast_healthchecker'
+__author__ = 'Pavlos Parissis'
+__version__ = '0.2.0'
+__copyright__ = 'Copyright 2015 Pavlos Parissis'
