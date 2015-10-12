f1deacd68c65670e7a8e985d673e0aafaffdb473
Pavlos Parissis <pavlos.parissis@gmail.com>
2015-10-13 00:41:33 +0200

    some updates on local_run

--- local_run.sh
+++ local_run.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 echo "--------create directory structure--------"
 mkdir -p "${PWD}"/var/var/log/anycast-healthchecker
+mkdir -p "${PWD}"/var/etc/bird.d
+touch "${PWD}"/var/etc/bird.d/anycast-prefixes.conf
 touch "${PWD}"/var/var/log/anycast-healthchecker/anycast-healthchecker.log
 touch "${PWD}"/var/var/log/anycast-healthchecker/stdout.log
 touch "${PWD}"/var/var/log/anycast-healthchecker/stderr.log
 mkdir -p "${PWD}"/var/etc/anycast-healthcheck.d
 mkdir -p "${PWD}"/var/var/run/anycast-healthchecker
 mkdir -p "${PWD}"/var/var/run/anycast-healthchecker
-mkdir -p "${PWD}"/var/etc/bird.d/anycast-prefixes.conf
 echo "-----------installing software------------"
 python3.4 setup.py install --user
 echo "-------------runing software--------------"
