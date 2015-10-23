unixsurfer/anycast_healthchecker

commit c469573c4029493d8d09e242d21b0847e0ba6b67

BUG:perform sanity check before we check if daemon is running

anycast_healthchecker/main.py
@@ -91,6 +91,12 @@ def main():
             print()
         sys.exit(0)
 
+    # Perform a sanity check on the configuration
+    if args['--check']:
+        configuration_check(config)
+        print("OK")
+        sys.exit(0)
+
     # Catch already running process and clean up stale pid file.
     pidfile = config['daemon']['pidfile']
     if os.path.exists(pidfile):
@@ -107,10 +113,6 @@ def main():
                 print("Cleaning stale pid file with pid:{}".format(pid))
                 os.unlink(pidfile)
 
-    # Perform a sanity check on the configuration
-    configuration_check(config)
-    if args['--check']:
-        sys.exit(0)
 
     # Map log level to numeric which can be accepted by loggers.
     numeric_level = getattr(logging, config['daemon']['loglevel'].upper(), None)

commit 68a3c87adfcae0359b81192199ff9bde32c60039

BUG: always perform sanity checks
This fixes regression introduced by c469573c where santity checks
were performed only when user asked for it.

anycast_healthchecker/main.py
@@ -92,8 +92,8 @@ def main():
         sys.exit(0)
 
     # Perform a sanity check on the configuration
+    configuration_check(config)
     if args['--check']:
-        configuration_check(config)
         print("OK")
         sys.exit(0)
 