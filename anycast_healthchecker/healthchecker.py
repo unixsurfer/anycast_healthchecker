unixsurfer/anycast_healthchecker

commit 37aa574b1159551ed8f57da6f303679eb591cdc3

Simplify the update of bird configuration
This change is the result of code review from @carlesnagy

anycast_healthchecker/healthchecker.py
@@ -138,11 +138,8 @@ class HealthChecker(object):
                 # all entries of the array constant need a trailing comma
                 # except the last one. A single element array doesn't need the
                 # trailing comma.
-                for prefix in prefixes[:-1]:
-                    bird_conf.write("{}{},\n".format(8 * ' ', prefix))
-                bird_conf.write("{}{}\n".format(8 * ' ',
-                                                prefixes[len(prefixes) - 1]))
-                bird_conf.write("{}];\n".format(4 * ' '))
+                bird_conf.write(',\n'.join(map(lambda p: ' '*8 + p, prefixes)))
+                bird_conf.write("{spaces}];\n".format(spaces=4 * ' '))
                 bird_conf.truncate()
                 bird_conf.close()
                 self.log.info("Bird configuration is updated")

commit eae4621876b804fd050071696f0016c071e107a3

Fix a regression introduced by 37aa574b1159
In 37aa574b1159551e we simplified the way we write IP prefixes in the
bird configuration by using a lambda to generate the list and join
them with comma and new-line character. But, the last element never
got the newline character and as a result a subsequent parsing of the
bird file skipped that IP prefix.

We fix this by adding a leading newline character on the next write.

anycast_healthchecker/healthchecker.py
@@ -139,7 +139,7 @@ class HealthChecker(object):
                 # except the last one. A single element array doesn't need the
                 # trailing comma.
                 bird_conf.write(',\n'.join(map(lambda p: ' '*8 + p, prefixes)))
-                bird_conf.write("{spaces}];\n".format(spaces=4 * ' '))
+                bird_conf.write("\n{spaces}];\n".format(spaces=4 * ' '))
                 bird_conf.truncate()
                 bird_conf.close()
                 self.log.info("Bird configuration is updated")