unixsurfer/anycast_healthchecker

commit 168f9aab5df1d5d08c1b806be1b9cddaedce38cd

Change the way threads pass operations
Threads were passing a tuple to the queue for informing main program
to either add or remove IP prefixes from the bird configuration.
In the tuple we stored the IP prefix, action(add or remove) and name
of the service being checked. That was a bit cumbersome approach.

This commit introduces 3 classes which handle the addition and removal
of IP prefixes. Below is the hierarchy of the classes:
BaseOperation
├──AddOperation
├──DeleteOperation

This change is the result of code review from @carlesnagy

anycast_healthchecker/servicecheck.py
@@ -10,6 +10,8 @@ import time
 from threading import Thread
 import shlex
 
+from anycast_healthchecker.utils import AddOperation, DeleteOperation
+
 
 class ServiceCheck(Thread):
     """Handles a check for a service.
@@ -181,9 +183,11 @@ class ServiceCheck(Thread):
                     # reloads when a service flaps between states.
                     if check_state != 'UP':
                         check_state = 'UP'
-                        self.action.put((self.name,
-                                         self.config['ip_prefix'],
-                                         'add'))
+                        operation = AddOperation(
+                            name=self.name,
+                            ip_prefix=self.config['ip_prefix'],
+                            log=self.log)
+                        self.action.put(operation)
                         self.log.info(
                             "Queued {}".format(self.config['ip_prefix']))
                 elif up_cnt < self.config['check_rise']:
@@ -202,9 +206,11 @@ class ServiceCheck(Thread):
                     # unnecessary bird reloads when a service flaps between states
                     if check_state != 'DOWN':
                         check_state = 'DOWN'
-                        self.action.put((self.name,
-                                         self.config['ip_prefix'],
-                                         'del'))
+                        operation = DeleteOperation(
+                            name=self.name,
+                            ip_prefix=self.config['ip_prefix'],
+                            log=self.log)
+                        self.action.put(operation)
                         self.log.info(
                             "Queued {}".format(self.config['ip_prefix']))
                 elif down_cnt < self.config['check_fail']:

anycast_healthchecker/healthchecker.py
@@ -68,17 +68,14 @@ class HealthChecker(object):
 
         self.log.debug("Initialize healthchecker")
 
-    def _update_bird_prefix_conf(self, health_action):
+    def _update_bird_prefix_conf(self, operation):
         """Updates BIRD configuration.
 
         Adds/removes entries from a list and updates generation time stamp.
         Main program will exit if configuration file cant be read/written.
 
         Arguments:
-            health_action (tuple): A 3 element tuple:
-            1st: The name of the thread (str)
-            2nd: ip_prefix (str)
-            3nd: Action to take, either 'add' or 'del' (str)
+            operation (obj): Either an AddOperation or DeleteOperation object
 
         Returns:
             True if BIRD configuration was updated otherwise False.
@@ -86,9 +83,6 @@ class HealthChecker(object):
         """
         conf_updated = False
         prefixes = []
-        name = health_action[0]
-        ip_prefix = health_action[1]
-        action = health_action[2]
         comment = ("# {} is a dummy IP Prefix. It should NOT be used and "
                    "REMOVED from the constant.".format(self.dummy_ip_prefix))
 
@@ -112,15 +106,8 @@ class HealthChecker(object):
             prefixes.insert(0, self.dummy_ip_prefix)
             conf_updated = True
 
-        if action == 'del' and ip_prefix in prefixes:
-            self.log.info("Withdrawing {} for {}".format(ip_prefix, name))
-            prefixes.remove(ip_prefix)
-            conf_updated = True
-        elif action == 'add' and ip_prefix not in prefixes:
-            self.log.info("Announcing {} for {}".format(ip_prefix, name))
-            prefixes.append(ip_prefix)
-            conf_updated = True
-
+        # Update the list of IP prefixes.
+        conf_updated = operation.update(prefixes)
         if not conf_updated:
             self.log.info("No updates for bird configuration")
             return conf_updated
@@ -215,14 +202,14 @@ class HealthChecker(object):
         # Stay running until we are stopped
         while True:
             # Fetch items from action queue
-            health_action = self.action.get(block=True)
-            self.log.info(("Returned an item from the queue for {} with "
-                           "IP prefix {} and action to {} from Bird "
-                           "configuration").format(health_action[0],
-                                                   health_action[1],
-                                                   health_action[2]))
+            operation = self.action.get(block=True)
+            self.log.info(("Returned an item from the queue for {n} with "
+                           "IP prefix {i} and action to {o} Bird "
+                           "configuration").format(n=operation.name,
+                                                   i=operation.ip_prefix,
+                                                   o=operation))
 
-            bird_updated = self._update_bird_prefix_conf(health_action)
+            bird_updated = self._update_bird_prefix_conf(operation)
             self.action.task_done()
             if bird_updated:
                 self._reload_bird()

anycast_healthchecker/utils.py
@@ -245,3 +245,38 @@ def get_ip_prefixes_from_bird(filename, die=True):
                 prefixes.append(line.rstrip(','))
 
     return prefixes
+
+
+class BaseOperation(object):
+    def __init__(self, name, ip_prefix, log):
+        self.name = name
+        self.ip_prefix = ip_prefix
+        self.log = log
+
+
+class AddOperation(BaseOperation):
+    def __str__(self):
+        return 'add to'
+
+    def update(self, prefixes):
+        if self.ip_prefix not in prefixes:
+            prefixes.append(self.ip_prefix)
+            self.log.info("Announcing {i} for {n}".format(
+                i=self.ip_prefix, n=self.name))
+            return True
+
+        return False
+
+
+class DeleteOperation(BaseOperation):
+    def __str__(self):
+        return 'delete from'
+
+    def update(self, prefixes):
+        if self.ip_prefix in prefixes:
+            prefixes.remove(self.ip_prefix)
+            self.log.info("Withdrawing {i} for {n}".format(
+                i=self.ip_prefix, n=self.name))
+            return True
+
+        return False

commit 3ddc1a5298a81f26c65c5672f46d510f7d040113

Fix a regression introduced by 168f9aab
168f9aab5df1 changed the way we send data from threads to main program.
We used to send a tuple but now we send an object which carries all the
information we need. In 168f9aab5df1 we forgot to send an object when
healthchecking was disabled, we were sending a tuple to the queue
but the consumer of the queue was waiting an objection, which resulted
to an AttributeError exception and termination of the daemon.

anycast_healthchecker/servicecheck.py
@@ -120,14 +120,20 @@ class ServiceCheck(Thread):
         if (self.config['check_disabled']
                 and self.config['on_disabled'] == 'withdraw'):
             self.log.info("Check is disabled and ip_prefix will be withdrawn")
-            self.action.put((self.name, self.config['ip_prefix'], 'del'))
+            operation = DeleteOperation(name=self.name,
+                                        ip_prefix=self.config['ip_prefix'],
+                                        log=self.log)
+            self.action.put(operation)
             self.log.info("{} in queue".format(self.config['ip_prefix']))
             self.log.info("Check is now permanently disabled")
             return True
         elif (self.config['check_disabled']
               and self.config['on_disabled'] == 'advertise'):
             self.log.info("Check is disabled, ip_prefix wont be withdrawn")
-            self.action.put((self.name, self.config['ip_prefix'], 'add'))
+            operation = AddOperation(name=self.name,
+                                     ip_prefix=self.config['ip_prefix'],
+                                     log=self.log)
+            self.action.put(operation)
             self.log.info("{} in queue".format(self.config['ip_prefix']))
             self.log.info("Check is now permanently disabled")
             return True