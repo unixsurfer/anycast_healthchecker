2a2224cde6c6a93fb558c01f7dddf25b6caaf032
Pavlos Parissis <pavlos.parissis@booking.com>
2015-11-22 15:20:58 +0100

    RELEASE 0.4.4 version

--- anycast_healthchecker/__init__.py
+++ anycast_healthchecker/__init__.py
@@ -5,5 +5,5 @@
 __title__ = 'anycast_healthchecker'
 __author__ = 'Pavlos Parissis'
 __license__ = 'Apache 2.0'
-__version__ = '0.4.3'
+__version__ = '0.4.4'
 __copyright__ = 'Copyright 2015 Pavlos Parissis'
