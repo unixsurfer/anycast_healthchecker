unixsurfer/anycast_healthchecker

commit ad54f9ed84c06effee47b52ab7f49f428a8d9050

Remove string interpolation within logging calls
From pylint documentation::

    logging-format-interpolation (W1202):
    Use % formatting in logging functions and pass the % parameters
    as arguments Used when a logging statement has a call form of
    “logging.<logging method>(format_string.format(format_args...))”.
    Such calls should use % formatting instead, but leave interpolation
    to the logging function by passing the parameters as arguments.

anycast_healthchecker/lib.py
@@ -251,27 +251,26 @@ class LoggerExt(object):
         except (requests.exceptions.Timeout,
                 requests.exceptions.ConnectionError,
                 requests.exceptions.RequestException) as error:
-            self.logger.warning("failed to send data to {s}: {e}".format(
-                s=self.server, e=error))
+            self.logger.warning("failed to send data to %s: %s",
+                                self.server, error)
         else:
             if req.status_code == 200:
                 try:
                     response = req.json()
                 except ValueError as error:
-                    self.logger.warning("failed to decode JSON response ({r}): "
-                                        "{e}".format(r=req.text, e=error))
+                    self.logger.warning("failed to decode JSON response (%s)"
+                                        ": %s", req.text, error)
                 else:
                     # a valid response(JSON) looks like
                     # {"Status":"OK","Responses":["OK"]}
                     if (response['Status'] == 'OK'
                             and 'OK' in response['Responses']):
-                        self.logger.debug('JSON data sent successfully')
+                        self.logger.debug("JSON sent successfully %s", data)
                     else:
-                        self.logger.warning("something went wrong when we sent "
-                                            "({d}) as response is ({r})".format(
-                                                d=data, r=req.text))
+                        self.logger.warning("something went wrong when we sent"
+                                            " (%s) as response from server "
+                                            "was (%s)", data, req.text)
             else:
                 self.logger.warning("failed to send JSON data, received HTTP "
-                                    "status code {s} with response content "
-                                    "({r})".format(s=req.status_code,
-                                                   r=req.text))
+                                    "status code %s with response content "
+                                    "(%s)", req.status_code, req.text)

commit b8f981bf886ff64ffd83047bf217051c269b26de

Remove the JSON blob from the log messages
ad54f9ed8 accidentally introduced the emitting of JSON blobs in the log
messages, which is very noisy and make logging parsing painful.

anycast_healthchecker/lib.py
@@ -280,7 +280,7 @@ class LoggerExt(object):
                     # {"Status":"OK","Responses":["OK"]}
                     if (response['Status'] == 'OK'
                             and 'OK' in response['Responses']):
-                        self.logger.debug("JSON sent successfully %s", data)
+                        self.logger.debug('JSON sent successfully')
                     else:
                         self.logger.warning("something went wrong when we sent"
                                             " (%s) as response from server "