e0b02a181c2d7ef7f6035a6baa5dcfb44d25d6cb
Pavlos Parissis <pavlos.parissis@gmail.com>
2016-11-06 19:29:17 +0100

    RELEASE 0.6.3 version

--- anycast_healthchecker/__init__.py
+++ anycast_healthchecker/__init__.py
@@ -11,7 +11,7 @@ A healthchecker for Anycasted services.
 __title__ = 'anycast_healthchecker'
 __author__ = 'Pavlos Parissis'
 __license__ = 'Apache 2.0'
-__version__ = '0.6.2'
+__version__ = '0.6.3'
 __copyright__ = 'Copyright 2015-2016 Pavlos Parissis'
 
 DEFAULT_OPTIONS = {
