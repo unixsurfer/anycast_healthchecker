8c801c23bfcf9a5711d66f53e5bc3c0a6d78c68a
Pavlos Parissis <pavlos.parissis@gmail.com>
2017-01-11 00:37:28 +0100

    Turn on bird6_keep_changes in example config

--- local_run.sh
+++ local_run.sh
@@ -46,7 +46,7 @@ http_server_timeout     = 0.1
 ipv4                    = true
 bird_conf               = ${TEST_DIR}/var/lib/anycast-healthchecker/anycast-prefixes.conf
 bird_variable           = ACAST_PS_ADVERTISE
-bird_keep_changes       = false
+bird_keep_changes       = true
 bird_changes_counter    = 6
 bird_reconfigure_cmd    = /usr/bin/sudo /usr/sbin/birdc configure
 dummy_ip_prefix         = 10.189.200.255/32
@@ -56,7 +56,7 @@ bird6_conf              = ${TEST_DIR}/var/lib/anycast-healthchecker/6/anycast-pr
 bird6_variable          = ACAST6_PS_ADVERTISE
 dummy_ip6_prefix        = 2001:db8::1/128
 bird6_reconfigure_cmd   = sudo /usr/sbin/birdc6 configure
-bird6_keep_changes      = false
+bird6_keep_changes      = true
 bird6_changes_counter   = 6
 EOT
 fi
