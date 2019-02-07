c7c04a2124e3eecf6e840109ae45149aed2a1569
Pavlos Parissis <pavlos.parissis@gmail.com>
2019-02-07 22:38:23 +0100

    Update year in __copyright__

--- anycast_healthchecker/__init__.py
+++ anycast_healthchecker/__init__.py
@@ -5,7 +5,7 @@ __title__ = 'anycast_healthchecker'
 __author__ = 'Pavlos Parissis'
 __license__ = 'Apache 2.0'
 __version__ = '0.8.1'
-__copyright__ = 'Copyright 2015-2018 Pavlos Parissis'
+__copyright__ = 'Copyright 2015-2019 Pavlos Parissis'
 
 PROGRAM_NAME = __title__.replace('_', '-')
 
